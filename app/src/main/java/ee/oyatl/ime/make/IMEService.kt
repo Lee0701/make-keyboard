19b16f75c886d23cbc07aa98310dc0e56d6a5e2b
Lee0701 <hgl1002@naver.com>
2023-07-05 01:56:43 +0900

    Add an empty IME service

---
+++ app/src/main/java/ee/oyatl/ime/make/IMEService.kt
@@ -0,0 +1,19 @@
+package ee.oyatl.ime.make
+
+import android.inputmethodservice.InputMethodService
+import android.view.View
+
+class IMEService: InputMethodService() {
+    override fun onCreate() {
+        super.onCreate()
+    }
+
+    override fun onCreateInputView(): View {
+        return super.onCreateInputView()
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+    }
+
+}
