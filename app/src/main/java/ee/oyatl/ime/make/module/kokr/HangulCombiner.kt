15ee54a8df45fe322871c54822554f0063fcbb40
Lee0701 <hgl1002@naver.com>
2024-08-01 16:20:14 +0900

    Add comments

--- app/src/main/java/ee/oyatl/ime/make/module/kokr/HangulCombiner.kt
+++ app/src/main/java/ee/oyatl/ime/make/module/kokr/HangulCombiner.kt
@@ -20,24 +20,29 @@ class HangulCombiner(
         }
     }
 
-    fun combine(current: State, input: Int): CombinedHangul {
+    fun combine(current: State, input: Int): Result {
+        // TODO: Needs to be extended to fit with non-BMP code
         val inputChar = input and 0xffff
         val inputExtra = input and 0xffff.inv()
         val cho = if(Hangul.isCho(inputChar)) Hangul.HangulJamo.Choseong(inputChar - 0x1100, inputExtra) else null
         val jung = if(Hangul.isJung(inputChar)) Hangul.HangulJamo.Jungseong(inputChar - 0x1161, inputExtra) else null
         val jong = if(Hangul.isJong(inputChar)) Hangul.HangulJamo.Jongseong(inputChar - 0x11a8, inputExtra) else null
+        // Non-hangul input
         if(cho == null && jung == null && jong == null) {
             val text = current.syllable.composed.toString() + inputChar.toChar().toString()
-            return CombinedHangul(text, State())
+            return Result(text, State())
         }
+        // Process automata
         val next = ordered.test(current.statusCode, cho?.ordinal, jung?.ordinal, jong?.ordinal)
         return if(next == 0) {
+            // Special status code 0: reset composition and start with the next char
             val state = State(input, State(), next, Hangul.HangulSyllable(cho, jung, jong))
-            CombinedHangul(current.syllable.composed, state)
+            Result(current.syllable.composed, state)
         } else {
+            // Set current state as the result
             val syllable = current.syllable + Hangul.HangulSyllable(cho, jung, jong)
             val state = State(input, current, next, syllable)
-            CombinedHangul("", state)
+            Result("", state)
         }
     }
 
@@ -48,11 +53,18 @@ class HangulCombiner(
         val syllable: Hangul.HangulSyllable = Hangul.HangulSyllable()
     )
 
-    data class CombinedHangul(
+    /**
+     * Result value for Hangul combiner
+     * includes information about the new text to commit and the next state
+     */
+    data class Result(
         val text: CharSequence,
         val state: State
     )
 
+    /**
+     * Miniature Hangul composition automata
+     */
     interface Automata {
         fun test(current: Int, choCode: Int?, jungCode: Int?, jongCode: Int?): Int
     }
