c20c433beaa6f613f255b0e51693f2d2e9e777a8
Automated Publisher <actions@users.noreply.github.com>
2024-08-18 18:28:31 +0900

    Bump version Sun Aug 18 18:28:31 KST 2024 7ae75e97e49ade8de7bd2748ce59a9db0cbfb053

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -12,8 +12,8 @@ android {
         applicationId = "ee.oyatl.ime.make"
         minSdk = 21
         targetSdk = 35
-        versionCode = 28
-        versionName = "20240818-28-eb090c9"
+        versionCode = 29
+        versionName = "20240818-29-7ae75e9"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
     }
